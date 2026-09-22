d14075df78d079fb6f4504324f9dac4ef38c45a1
Адиятова Софья <piligrimix24@gmail.com>
2026-09-22 22:50:48 +0500

    Spinning cubes hw2

---
+++ Assets/Scripts/CubesSpinning.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class CubesSpinning : MonoBehaviour
+{
+    [SerializeField] private GameObject cubePrefab;
+    [Header("Cubes Settings")]
+    [Range(0f, 7f)]
+    [SerializeField] private float spinningRadius = 2f;
+    [Range(40f, 160f)]
+    [SerializeField] private float spinningSpeed = 1f;
+    [SerializeField] private int cubeCount = 4;
+    [SerializeField] private bool rotateClockWise = true;
+    [SerializeField] private bool arrangeEvenly = true;
+    [Range(20f, 50f)]
+    [SerializeField] private float chainStep = 30f;
+
+    private GameObject[] cubes;
+    private float[] startAngles;
+    //==================mew========================================
+    private void Start()
+    {
+        if (cubePrefab != null)
+        {
+            cubes = new GameObject[cubeCount];
+            startAngles = new float[cubeCount];
+            float angleStep = 360f / cubeCount;
+
+            for (int i = 0; i < cubeCount; i++)
+            {
+                GameObject newCube = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+                newCube.transform.SetParent(transform);
+                cubes[i] = newCube;
+                startAngles[i] = i * angleStep;
+
+                startAngles[i] = arrangeEvenly ? i * angleStep : i * chainStep;
+                SetCubePosition(newCube, startAngles[i]);
+            }
+        }
+        else
+        {
+            Debug.LogError("Не назначен префаб");
+        }
+    }
+    private void Update()
+    {
+        float dir = rotateClockWise ? -1f : 1f;
+        transform.Rotate(0f, spinningSpeed * dir * Time.deltaTime, 0f);
+        for (int i = 0; i < cubeCount; i++)
+        {
+            SetCubePosition(cubes[i], startAngles[i]);
+        }
+    }
+    private void SetCubePosition(GameObject cube, float angleDeg)
+    {
+        float rad = angleDeg * Mathf.Deg2Rad;
+        float x = Mathf.Cos(rad) * spinningRadius;
+        float z = Mathf.Sin(rad) * spinningRadius;
+        cube.transform.localPosition = new Vector3(x, 0f, z);
+    }
+}
